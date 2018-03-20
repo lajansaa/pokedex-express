4ebeff2abe6babc681a228aee74ac17e4c17306b
Isa <lajansa@hotmail.com>
2018-03-20 22:53:17 +0800

    submission (isa)

--- index.js
+++ index.js
@@ -1,40 +1,64 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-
-// const jsonfile = require('jsonfile');
-
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
-
-// Init express app
+const hbsHelpers = require('handlebars-helpers');
+const jsonfile = require('jsonfile');
 const app = express();
 
 // Set handlebars to be the default view engine
 app.engine('handlebars', handlebars.create().engine);
 app.set('view engine', 'handlebars');
 
-/**
- * ===================================
- * Routes
- * ===================================
- */
-
-app.get('/names/:pokemon', (request, response) => {
-  // send response with some data (a string)
-  response.send(request.params.pokemon);
+let pokemonArr;
+jsonfile.readFile('pokedex.json', (err, obj) => {
+  pokemonArr = obj.pokemon;
 });
 
-app.get('/', (request, response) => {
-  // send response with some data (a HTML file)
-  response.render('home');
-});
+let requestLocation = '/*';
 
-/**
- * ===================================
- * Listen to requests on port 3000
- * ===================================
- */
-app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+let handleRequest = (request, response) => {
+  let input = request.params[0].split("/");
+  let search = input[0];
+  let searchTerm = input[1];
+
+  switch(search) {
+    case "":
+      const allPokemonNames = {"name": []};
+      pokemonArr.forEach((pokemon) => {
+        allPokemonNames.name.push(pokemon.name);
+      });
+      
+      response.render('home', allPokemonNames);
+      break;
+
+    case "names":
+      let pokemonFound = false;
+      for (let i = 0; i < pokemonArr.length; i++) {
+        if (pokemonArr[i].name == searchTerm) {
+          pokemonFound = true;
+          pokemonArr[i]["pokemonFound"] = pokemonFound;
+          const searchPokemon = {"info": pokemonArr[i]};
+          response.render('pokemon_info', searchPokemon);
+          return;
+        }
+      }
+      if (pokemonFound == false) {
+        request.params["pokemonFound"] = pokemonFound;
+        response.render('pokemon_info', request.params);
+      }
+      break;
+
+    case "types":
+      const pokemonTypes = {"type_name": searchTerm,
+                       "pokemon_name": []
+                      };
+      pokemonArr.forEach((pokemon) => {
+        if (pokemon.type.includes(searchTerm)) {
+         pokemonTypes.pokemon_name.push(pokemon.name);
+        }
+      });
+      response.render('pokemon_type', pokemonTypes);
+      break;
+  }
+}
+
+app.get(requestLocation, handleRequest).listen(3000);
